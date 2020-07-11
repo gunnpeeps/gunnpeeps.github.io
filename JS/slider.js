c987feb8f4924d2dfe7e6caf0bb3a991b68e34a7
omahesh <2ommahesh@gmail.com>
2020-07-11 01:39:01 -0700

    Added arrows to slider.

--- JS/slider.js
+++ JS/slider.js
@@ -1,83 +1,92 @@
-
 class Slider {
-    constructor(sliderClass){
+  constructor(sliderClass) {
 
-        this.currSlide = 2;
-        this.timeBetweenSlides = 6000;
-        this.slideTime = 1000;
+    this.currSlide = 2;
+    this.timeBetweenSlides = 8000;
+    this.slideTime = 1000;
 
-        this.slides = $(`.${sliderClass}>img`);
-        this.setMargins(this.currSlide);
+    this.slides = $(`.${sliderClass}>img`);
+    this.setMargins(this.currSlide);
 
-        setInterval(() => {
-            if(!this.nextSlideQd){
-                this.nextSlide();
-                this.nextSlideQd = true;
-            }
-        }, this.timeBetweenSlides);
+    setInterval(() => {
+      if (!this.nextSlideQd) {
+        this.nextSlide();
+        this.nextSlideQd = true;
+      }
+    }, this.timeBetweenSlides);
 
+    this.nextSlideQd = false;
+
+    return this;
+  }
+
+  setMargins(n) {
+    for (let i = 0; i < this.slides.length; i++) {
+      this.slides.eq(i).css({
+        marginLeft: ((i - n) * 100) + "vw"
+      })
+    }
+  }
+
+  animateMargins(n) {
+    for (let i = 0; i < this.slides.length; i++) {
+      this.slides.eq(i).animate({
+        marginLeft: ((i - n) * 100) + "vw"
+      }, this.slideTime, () => {
         this.nextSlideQd = false;
+      })
+    }
+  }
 
-        return this;
+  nextSlide() {
+
+    if (this.currSlide >= this.slides.length - 1) {
+      this.currSlide = 0;
+      this.setMargins(this.currSlide);
     }
 
-    setMargins(n){
-        for(let i = 0; i < this.slides.length; i++){
-            this.slides.eq(i).css({
-                marginLeft: ((i - n) * 100) + "vw"
-            })
-        }
+    this.currSlide++;
+    this.animateMargins(this.currSlide);
+
+  }
+
+  goToSlide(n) {
+    if (!this.nextSlideQd) {
+      this.nextSlideQd = true;
+      this.currSlide = n;
+      this.animateMargins(n);
+    }
+  }
+
+  prevSlide() {
+
+    if (this.currSlide <= 0) {
+      this.currSlide = this.slides.length - 1;
+      this.setMargins(this.currSlide);
     }
 
-    animateMargins(n) {
-        for (let i = 0; i < this.slides.length; i++) {
-            this.slides.eq(i).animate({
-                marginLeft: ((i - n) * 100) + "vw"
-            }, this.slideTime, () => {
-                this.nextSlideQd = false;
-            })
-        }
-    }
+    this.currSlide--;
+    this.animateMargins(this.currSlide);
 
-    nextSlide(){
 
-        if (this.currSlide >= this.slides.length - 1) {
-            this.currSlide = 0;
-            this.setMargins(this.currSlide);
-        }
-
-        this.currSlide++;
-        this.animateMargins(this.currSlide);
-        
-    }
-
-    goToSlide(n){
-        if(!this.nextSlideQd){
-            this.nextSlideQd = true;
-            this.currSlide = n;
-            this.animateMargins(n);
-        }
-    }
-
-    prevSlide(){
-
-        if (this.currSlide <= 0) {
-            this.currSlide = this.slides.length-1;
-            this.setMargins(this.currSlide);
-        }
-
-        this.currSlide--;
-        this.animateMargins(this.currSlide);
-        
-
-    }
+  }
 
 }
 
-let slider1;
+var slider1;
 
-$(function () {
+$(function() {
 
-    slider1 = new Slider("slider-1")
+  slider1 = new Slider("slider-1");
+
+  leftArr = $(".left-arr");
+  rightArr = $(".right-arr");
+
+  leftArr.on("click", () => {
+    slider1.prevSlide();
+  });
+  rightArr.on("click", () => {
+    slider1.nextSlide();
+  });
 
 });
