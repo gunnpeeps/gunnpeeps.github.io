4943c9e2c172caac1c8cdf3758916ed942ddec49
Roger Fan <rogerjoeyfan@gmail.com>
2020-07-11 10:36:54 -0700

    test

--- JS/menu.js
+++ JS/menu.js
@@ -6,10 +6,12 @@ $(() => {
             $(".nav-bar a div").fadeOut(200);
             $("#menu-icon").css("transform", "translate(-50%, -50%) rotate(0deg)");
             $("#header-logo").css("width","120px");
+            $("#sign-in-google").fadeIn(200);
         } else {
             $(".nav-bar a div").fadeIn(200);
             $("#menu-icon").css("transform", "translate(-50%, -50%) rotate(360deg)")
             $("#header-logo").css("width", "0px");
+            $("#sign-in-google").fadeOut(200);
         }
         globals.menuShown = !globals.menuShown;
     })
