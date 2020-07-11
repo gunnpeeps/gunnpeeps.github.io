3eae946e8643dc9353ea16dd7bad169b62c24efc
Roger Fan <rogerjoeyfan@gmail.com>
2020-07-10 20:13:36 -0700

    sick slider changes

--- JS/slider.js
+++ JS/slider.js
@@ -30,46 +30,55 @@ class Slider {
     }
 
     animateMargins(n) {
-        for (let i = 0; i < this.slides.length; i++) {
-            this.slides.eq(i).animate({
-                marginLeft: ((i - n) * 100) + "vw"
-            }, this.slideTime, () => {
-                this.nextSlideQd = false;
-            })
+        if (!this.nextSlideQd) {
+            this.nextSlideQd = true;
+            for (let i = 0; i < this.slides.length; i++) {
+                this.slides.eq(i).animate({
+                    marginLeft: ((i - n) * 100) + "vw"
+                }, this.slideTime, () => {
+                    this.nextSlideQd = false;
+                })
+            }
         }
     }
 
     nextSlide(){
+        
+        if(!this.nextSlideQd){
+        
+            if (this.currSlide >= this.slides.length - 1) {
+                this.currSlide = 0;
+                this.setMargins(this.currSlide);
+            }
 
-        if (this.currSlide >= this.slides.length - 1) {
-            this.currSlide = 0;
-            this.setMargins(this.currSlide);
+            this.currSlide++;
+            this.animateMargins(this.currSlide);
+            this.nextSlideQd = true;
         }
-
-        this.currSlide++;
-        this.animateMargins(this.currSlide);
         
     }
 
     goToSlide(n){
         if(!this.nextSlideQd){
-            this.nextSlideQd = true;
             this.currSlide = n;
             this.animateMargins(n);
+            this.nextSlideQd = true;
         }
     }
 
     prevSlide(){
+        if (!this.nextSlideQd) {
+            if (this.currSlide <= 0) {
+                this.currSlide = this.slides.length-1;
+                this.setMargins(this.currSlide);
+                
+            }
 
-        if (this.currSlide <= 0) {
-            this.currSlide = this.slides.length-1;
-            this.setMargins(this.currSlide);
+            this.currSlide--;
+            this.animateMargins(this.currSlide);
+            
+            this.nextSlideQd = true;
         }
-
-        this.currSlide--;
-        this.animateMargins(this.currSlide);
-        
-
     }
 
 }
@@ -79,5 +88,11 @@ let slider1;
 $(function () {
 
     slider1 = new Slider("slider-1")
+    $(".left-arr").click(() => {
+        slider1.prevSlide();
+    })
+    $(".right-arr").click(() => {
+        slider1.nextSlide();
+    })
 
 });
